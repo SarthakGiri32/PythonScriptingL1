45c2f57d93d24d7ac2a2903210137bab02a2cec3
SarthakGiri32 <giri.sarthak1@gmail.com>
2021-11-23 18:04:28 +0530

    Adding string_test.py

---
+++ string_test.py
@@ -0,0 +1,9 @@
+strValue = "Hello! World"
+
+print(strValue)
+print(strValue[0])
+print(strValue[2: 5])
+print(strValue[2:])
+print(strValue[-2::-1])
+print((strValue + " ") * 2)
+print(strValue + "TEST")
